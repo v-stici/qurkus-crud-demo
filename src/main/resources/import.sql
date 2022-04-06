8fc9c6cb556c4e5bc7250b18f25fd670e7ed4bb4
UNIFUN - Vlad Stici <v.stici@unifun.local>
2022-04-06 17:05:48 +0300

    minor update

---
+++ src/main/resources/import.sql
@@ -0,0 +1,9 @@
+insert into home (address, year)
+values ('str. Stefan cel Mare', 2000),
+		('bd Dacia', 2010),
+		('str. Stefan cel Mare', 1990);
+
+insert into apartment (haveInternet, phone, rooms, size, home_id)
+    values (1, '12-123', 4, 60, 1);
+
+
